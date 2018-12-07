e4d99a6595263fe02b281464cc25a18b0a37f571
jeanp27 <42422097+jeanp27@users.noreply.github.com>
2018-12-07 15:36:56 -0400

    Add files via upload

--- menu.php
+++ menu.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Tomato Company - Free CSS Layout, Website Template</title>
+<title> Pizza SICI </title>
 <meta name="keywords" content="" />
 <meta name="description" content="" />
 <link href="templatemo_style.css" rel="stylesheet" type="text/css" />
@@ -14,67 +14,145 @@
   <div id="templatemo_top">
     <div id="templatemo_title">PIZZERIA <span>SICI</span></div>
       <div class="templatemo_menu">
-    <ul>
-      <li><a href="index.php">INICIO</a></li>
-      <li><a href="#" style="color:#9c9c9c">MENU</a></li>
-      <li><a href="contact.php">CONTACTANOS</a></li>
-    </ul>
-  </div>
-  </div>
-  <div id="templatemo_header">
-    <div id="templatemo_header_slogan">      
-      <p><br><br><br>¡ Este es el sitio web de pizzería SICI ! Aquí iría el slogan... ¡si se me ocurriera uno!</p>
+        <ul>
+          <li><a href="index.php">INICIO</a></li>
+          <li><a href="#" style="color:#9c9c9c">MENU</a></li>
+          <li><a href="contact.php">CONTACTANOS</a></li>
+        </ul>
+      </div>
     </div>
-  </div>
+    <div id="templatemo_header">
+      <div id="templatemo_header_slogan">      
+        <p><br><br><br>¡ Este es el sitio web de pizzería SICI ! Aquí iría el slogan... ¡si se me ocurriera uno!</p>
+      </div>
+    </div>
     <div id="templatemo_content" class="style2">
       <div id="templatemo_main_content">
         <div><i id="icon" class="fa fa-shopping-cart"></i></div>          
         <h2>Menú</h2>
-        <ul id="img_list">
-            <li id="imgL">
-                <img src="images/p_queso.jpg" width=200px height=200px />
-                <h3>Pizza de Queso</h3>
-                <p>Precio: $3.00<br>
-                  Ingredientes
-                </p>
-                <a class="button">Add to cart</a>
-            </li>
-                
-            <li id="imgR">
-                <img src="images/p_pepperoni.jpg" />
-                <h3>Pizza de Pepperoni</h3>
-                <p>Precio: $3.50<br>
-                  Ingredientes
-                </p>
-                <a class="button">Add to cart</a>
-            </li>
-            
-            <li id="imgL">
-                <img src="images/p_supreme.jpg" />
-                <h3>Pizza Suprema</h3>
-                <p>Precio: $6.00<br>
-                  Ingredientes</p>
-                <a class="button">Add to cart</a>
-            </li>
-            
-            <li id="imgR">
-                <img src="images/p_custom.png" />
-                <h3>Pizza Propia</h3>
-                <p>Precio: Varía<br>
-                  Ingredientes Varían</p>
-                <a class="button">Add to cart</a>
-            </li>
-        </ul>
+
+        <div class="checkbox">
+          <label>Queso &nbsp;&nbsp; <input type="checkbox" value=""></label>
+        </div>
+        <div class="form-group">
+          <label for="sel1">Salsa:</label>
+          <select class="form-control" id="sel1">
+            <option> Nada </option>
+            <option> Tomate </option>
+            <option> Marinara </option>
+            <option> Alfredo </option>
+            <option> Guayaba </option>
+          </select>
+        </div>
+        <br>
+        <h3> Toppings </h3>
+        <div class="row">
+          <div class="column">
+            <div class="form-group">
+              <label for="sel1">Carne 1:</label>
+              <select class="form-control" id="sel1">
+                <option> Nada </option>
+                <option> Pepperoni </option>
+                <option> Pollo </option>
+                <option> Bacon </option>
+                <option> Anchoas </option>
+                <option> Chorizo </option>
+                <option> Salami </option>
+              </select>
+            </div> 
+
+            <div class="form-group">
+              <label for="sel1">Carne 2:</label>
+              <select class="form-control" id="sel1">
+                <option> Nada </option>
+                <option> Pepperoni </option>
+                <option> Pollo </option>
+                <option> Bacon </option>
+                <option> Anchoas </option>
+                <option> Chorizo </option>
+                <option> Salami </option>
+              </select>
+            </div> 
+
+            <div class="form-group">
+              <label for="sel1">Carne 3:</label>
+              <select class="form-control" id="sel1">
+                <option> Nada </option>
+                <option> Pepperoni </option>
+                <option> Pollo </option>
+                <option> Bacon </option>
+                <option> Anchoas </option>
+                <option> Chorizo </option>
+                <option> Salami </option>
+              </select>
+            </div>
+          </div>
+
+          <div class="column">
+            <div class="form-group">
+              <label for="sel1">Otros 1:</label>
+              <select class="form-control" id="sel1">
+                <option> Nada </option>
+                <option> Cebolla </option>
+                <option> Setas </option>
+                <option> Pimiento Verde </option>
+                <option> Aceitunas Negras </option>
+                <option> Aceitunas Verdes </option>
+                <option> Orégano </option>
+                <option> Ajo </option>
+                <option> Piña </option>
+              </select>
+            </div> 
+
+            <div class="form-group">
+            <label for="sel1">Otros 1:</label>
+              <select class="form-control" id="sel1">
+                <option> Nada </option>
+                <option> Cebolla </option>
+                <option> Setas </option>
+                <option> Pimiento Verde </option>
+                <option> Aceitunas Negras </option>
+                <option> Aceitunas Verdes </option>
+                <option> Orégano </option>
+                <option> Piña </option>
+              </select>
+            </div> 
+
+            <div class="form-group">
+            <label for="sel1">Otros 1:</label>
+              <select class="form-control" id="sel1">
+                <option> Nada </option>
+                <option> Cebolla </option>
+                <option> Setas </option>
+                <option> Pimiento Verde </option>
+                <option> Aceitunas Negras </option>
+                <option> Aceitunas Verdes </option>
+                <option> Orégano </option>
+                <option> Piña </option>
+              </select>
+            </div>
+          </div>
+        </div>
+        
+        <br><br>
+
+        <div class="checkbox">
+          <label><input type="checkbox" value=""> Aceite de Oliva </label>
+        </div>
+        <div class="checkbox">
+          <label><input type="checkbox" value=""> Sal </label>
+        </div>
+        <a class="button">Add to cart</a>
       </div>
     </div>
-     <div id="templatemo_footer">
-       <div id="templatemo_footer_left">Copyright © Pizzería SICI <!-- Credit: www.templatemo.com --></div>
-       <div id="templatemo_footer_right">
-         <strong>Llámanos:</strong><br />
-           Tel: 000-100-1000<br />
-           Email: pizza&#64;sici.com<br />
-       </div>
-     </div>
+    <div id="templatemo_footer">
+      <div id="templatemo_footer_left">Copyright © Pizzería SICI <!-- Credit: www.templatemo.com --></div>
+      <div id="templatemo_footer_right">
+        <strong>Llámanos:</strong><br />
+          Tel: 000-100-1000<br />
+          Email: pizza&#64;sici.com<br />
+      </div>
+    </div>
 </div>
 <!-- templatemo 033 tomato -->
 <!-- 
