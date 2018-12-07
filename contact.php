9817a02b9119f7b2bc074445531af593b9a386f5
jeanp27 <42422097+jeanp27@users.noreply.github.com>
2018-12-07 11:50:11 -0400

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Tomato Company - Free CSS Layout, Website Template</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="templatemo_style.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+<div id="templatemo_container">
+  <div id="templatemo_top">
+    <div id="templatemo_title">PIZZERIA <span>SICI</span></div>
+      <div class="templatemo_menu">
+    <ul>
+      <li><a href="index.html">INICIO</a></li>
+      <li><a href="menu.html">MENU</a></li>
+      <li><a href="#" style="color:#9c9c9c">CONTACTANOS</a></li>
+    </ul>
+  </div>
+  </div>
+  <div id="templatemo_header">
+    <div id="templatemo_header_slogan">      
+      <p><br><br><br>¡ Este es el sitio web de pizzería SICI ! Aquí iría el slogan... ¡si se me ocurriera uno!</p>
+    </div>
+  </div>
+    <div id="templatemo_content" class="style1">
+      <div id="templatemo_content_left">
+          <h2>LOCALIZACION</h2>
+          <p>Avenida Santiago de los Caballeros<br>
+          Ponce, 00734 </p>
+        <h2>MAPA</h2>
+        <p>Aquí va a haber una mapa de Google Maps</p>
+      </div>
+      <div id="templatemo_content_right" class="contact">
+        <h1>HORARIO</h1>
+        <p> Lunes a Jueves: 10:00 a.m. - 8:00 p.m. <br><br>
+            Viernes: 10:00 a.m. - 11:00 p.m. <br><br>
+            Sábado: 12:00 m. - 10:00 p.m. <br><br>
+            Domingo: 3:00 p.m. - 12:00 p.m. <br><br><br> </p>
+        <h1>TELEFONO</h1>
+        <p> 000-100-1000<br><br><br></p>
+        <h1>E-MAIL</h1>
+        <p> pizza&#64;sici.com<br></p>
+      </div>
+    </div>
+     <div id="templatemo_footer">
+       <div id="footer_center">Copyright © Pizzería SICI</div>
+     </div>
+</div>
+<!-- templatemo 033 tomato -->
+<!-- 
+Tomato Template 
+http://www.templatemo.com/preview/templatemo_033_tomato 
+-->
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Tomato Company - Free CSS Layout, Website Template</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="templatemo_style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="templatemo_container">
+  <div id="templatemo_top">
+    <div id="templatemo_title">PIZZERIA <span>SICI</span></div>
+      <div class="templatemo_menu">
+    <ul>
+      <li><a href="#" style="color:#9c9c9c">INICIO</a></li>
+      <li><a href="menu.html">MENU</a></li>
+      <li><a href="contact.html">CONTACTANOS</a></li>
+    </ul>
+  </div>
+  </div>
+  <div id="templatemo_header">
+    <div id="templatemo_header_slogan">      
+      <p><br><br><br>¡ Este es el sitio web de pizzería SICI ! Aquí iría el slogan... ¡si se me ocurriera uno!</p>
+    </div>
+  </div>
+    <div id="templatemo_content" class="style1">
+      <div id="templatemo_content_left">
+        <h2>¿ Qué hacemos ?</h2>
+        <p><strong>¡Pizzas de calidad!</strong><br>
+          Aquí en la pizzería SICI nos enfocamos en brindarle las mejores pizzas, al mejor precio. </p>
+        <p><strong>¿Por qué un sitio web?</strong><br>
+          De esta forma tendrá otra opción para contactarnos. Además, tiene las opciones de menú a su alcance para ver con qué acompañar las pizzas.</p>
+        <h2>NOTICIAS</h2>
+        <h3>Publicada: 26-12-2020</h3>
+        <p> ¡Prueba nuestra nueva pizza, la Pizza Rick! Está probado científicamente que esta pizza incrementará su IQ. Disponible por tiempo limitado, ¡Ordénala ya! </p>
+        <h3>Publicada: 18-12-2020</h3>
+        <p>¡La Small New Yorker estará disponible por $4.99 sólo por tiempo limitado!</p>
+      </div>
+      <div id="templatemo_content_right">
+        <h1>NUESTRA HISTORIA</h1>
+        <ul>
+          <li><strong>2020:</strong> Creamos una pizza que incrementa tu IQ. </li>
+          <li><strong>2018:</strong> Diseñamos el sitio web para nuestra compañía </li>
+          <li><strong>2013:</strong> Nos reconocieron como la pizzería del año. </li>
+          <li><strong>2009:</strong> Se vendió el primer millón de pizzas. </li>
+          <li><strong>2005:</strong> Se fundó la pizzería de SICI. </li>
+        </ul>
+        <h1>MISION</h1>
+        <p> Proveer las mejores pizzas en Puerto Rico.</p>
+        <h1>VISION</h1>
+        <p> Cubrir a Puerto Rico con nuestras pizzas.</p>
+      </div>
+    </div>
+     <div id="templatemo_footer">
+       <div id="templatemo_footer_left">Copyright © Pizzería SICI <!-- Credit: www.templatemo.com --></div>
+       <div id="templatemo_footer_right">
+         <strong>Llámanos:</strong><br />
+           Tel: 000-100-1000<br />
+           Email: pizza&#64;sici.com<br />
+       </div>
+     </div>
+</div>
+<!-- templatemo 033 tomato -->
+<!-- 
+Tomato Template 
+http://www.templatemo.com/preview/templatemo_033_tomato 
+-->
+</body>
+</html>
